ea155b877e23d56e704de4c15a810e02d9a8fe7e
creindle <creindle@hawaii.edu>
2016-12-29 20:59:11 -1000

    Player can move now

--- js/game.js
+++ js/game.js
@@ -3,6 +3,9 @@
   const GAME_HEIGHT = 600;
   const GAME_CONTAINER_ID = 'game';
   const GFX = 'gfx';
+  let player;
+  const INITIAL_MOVESPEED = 20;
+  let cursors;
 
   //Ecma script, _ = ()
 
@@ -10,14 +13,33 @@
     game.load.spritesheet(GFX, 'assets/shmup-spritesheet-140x56-28x28-tile.png', 28, 28);
   };
 
-  let player;
-
   const create = _ => {
-    player = game.add.sprite(100, 100, GFX, 8);
+    cursors = game.input.keyboard.createCursorKeys();
+    player = game.add.sprite(210, 500, GFX, 8);
+    player.moveSpeed = INITIAL_MOVESPEED;
+  };
+
+  const handlePlayerMovement = _ => {
+    switch( true ){
+      case cursors.left.isDown:
+        player.x -= player.moveSpeed;
+        break;
+      case cursors.right.isDown:
+        player.x += player.moveSpeed;
+        break;
+    }
+    switch( true ){
+      case cursors.down.isDown:
+        player.y += player.moveSpeed;
+        break;
+      case cursors.up.isDown:
+        player.y -= player.moveSpeed;
+        break;
+    }
   };
 
   const update = _ => {
-
+    handlePlayerMovement();
   };
 
   const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
