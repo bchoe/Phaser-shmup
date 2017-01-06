ccc0e43b4eabca0887de1bcc395df9ae1529528f
creindle <creindle@hawaii.edu>
2017-01-05 20:03:30 -1000

    End commits

--- js/game.js
+++ js/game.js
@@ -3,40 +3,38 @@
   const GAME_HEIGHT = 600;
   const GAME_CONTAINER_ID = 'game';
   const GFX = 'gfx';
-  const INITIAL_MOVESPEED = 10;
-  const PLAYER_BULLET_SPEED = 1;
+  const INITIAL_MOVESPEED = 20;
+  const PLAYER_BULLET_SPEED = 6;
   const ENEMY_SPAWN_FREQ = 100; // higher is less frequent
-  const ENEMY_SPEED = 4.5;
-  const removeBullet = bullet => bullet.destroy();
-  const destroyEnemy = enemy => enemy.kill();
+  const ENEMY_SPEED = 2;
+  const ENEMY_FIRE_FREQ = 30; // higher is less frequent
+  const ENEMY_BULLET_ACCEL = 5;
   let player;
   let cursors;
   let playerBullets;
   let enemies;
 
-  //Ecma script, _ = ()
-
   const preload = _ => {
     game.load.spritesheet(GFX, 'assets/shmup-spritesheet-140x56-28x28-tile.png', 28, 28);
   };
 
+  const handlePlayerFire = _ => {
+    playerBullets.add( game.add.sprite(player.x, player.y, GFX, 7) );
+  };
+
   const create = _ => {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     cursors = game.input.keyboard.createCursorKeys();
     cursors.fire = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
     cursors.fire.onUp.add( handlePlayerFire );
-    player = game.add.sprite(210, 500, GFX, 8);
+
+    player = game.add.sprite(215, 500, GFX, 8);
     player.moveSpeed = INITIAL_MOVESPEED;
     playerBullets = game.add.group();
     enemies = game.add.group();
+    enemyBullets = game.add.group();
+    enemyBullets.enableBody = true;
   };
-
-
-  const randomlySpawnEnemy = _ => {
-    if( Math.floor(Math.random()*ENEMY_SPAWN_FREQ) === 0 ){
-      let randomX = Math.floor( Math.random()*GAME_WIDTH );
-      enemies.add( game.add.sprite(randomX, -24, GFX, 0) );
-    }
-  }
 
   const handlePlayerMovement = _ => {
     let movingH = Math.sqrt(2);
@@ -65,16 +63,41 @@
     }
   };
 
-  const handlePlayerFire = _ => {
-    playerBullets.add( game.add.sprite(player.x, player.y, GFX, 7) );
+  const handleBulletAnimations = _ => {
+    playerBullets.children.forEach( bullet => bullet.y -= PLAYER_BULLET_SPEED );
+    enemyBullets.children.forEach( bullet =>  {
+      game.physics.arcade.accelerateToObject(bullet, player, ENEMY_BULLET_ACCEL);
+    });
   };
 
-  const handleBulletAnimations1 = _ => {
-    playerBullets.children.forEach( bullet => bullet.y -= PLAYER_BULLET_SPEED );
+  const randomlySpawnEnemy = _ => {
+    if( Math.floor(Math.random()*ENEMY_SPAWN_FREQ) === 0 ){
+      let randomX = Math.floor( Math.random()*GAME_WIDTH );
+      enemies.add( game.add.sprite(randomX, -24, GFX, 0) );
+    }
   };
 
   const handleEnemyActions = _ => {
     enemies.children.forEach( enemy => enemy.y += ENEMY_SPEED );
+    enemies.children.forEach( enemy => randomEnemyFire(enemy));
+  };
+
+  const removeBullet = bullet => bullet.destroy();
+
+  const destroyEnemy = enemy => enemy.kill();
+
+  const gameOver = _ => {
+    game.state.destroy();
+    game.add.text(150, 200, 'O NOES', { fill: '#FFFFFF' });
+    let playAgain = game.add.text(120, 300, 'Play Again', { fill: '#FFFFFF' });
+    playAgain.inputEnabled = true;
+    playAgain.events.onInputUp.add(_ => {
+      window.location.reload();
+    });
+  };
+
+  const handlePlayerHit = _ => {
+    gameOver();
   };
 
   const handleCollisions = _ => {
@@ -91,6 +114,7 @@
 
       enemiesHit.forEach( destroyEnemy );
     }
+
     // check if enemies hit the player
     enemiesHit = enemies.children
       .filter( enemy => enemy.overlap(player) );
@@ -100,29 +124,45 @@
 
       enemiesHit.forEach( destroyEnemy );
     }
+
+    // check if enemy bullets hit the player
+    let enemyBulletsLanded = enemyBullets.children
+      .filter( bullet => bullet.overlap(player) );
+
+    if( enemyBulletsLanded.length > 0 ){
+      handlePlayerHit(); // count as one hit
+      enemyBulletsLanded.forEach( removeBullet );
+    }
+
   };
 
-  const gameOver = _ => {
-    game.state.destroy();
-    game.add.text(180, 200, 'O NOES', { fill: '#FFFFFF' });
-  };
-
-  const handlePlayerHit = _ => {
-    gameOver();
+  const randomEnemyFire = enemy => {
+    if( Math.floor(Math.random()*ENEMY_FIRE_FREQ) === 0 ){
+      let enemyBullet = game.add.sprite(enemy.x, enemy.y, GFX, 9);
+      enemyBullet.checkWorldBounds = true;
+      enemyBullet.outOfBoundsKill = true;
+      enemyBullets.add( enemyBullet );
+    }
   };
 
   const cleanup = _ => {
     playerBullets.children
       .filter( bullet => bullet.y < 0 )
       .forEach( bullet => bullet.destroy() );
+    enemies.children
+      .filter( enemy => enemy.y > GAME_HEIGHT || !enemy.alive )
+      .forEach( enemy => enemy.destroy() );
+    enemyBullets.children
+      .filter( bullet => !bullet.alive )
+      .forEach( bullet => bullet.destroy() );
   };
 
   const update = _ => {
     handlePlayerMovement();
-    handleBulletAnimations1();
+    handleBulletAnimations();
     handleEnemyActions();
-    randomlySpawnEnemy();
     handleCollisions();
+    randomlySpawnEnemy();
 
     cleanup();
   };
